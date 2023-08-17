9022cd9a0bbe02160c36a538b5345c55a6f95655
Arturo Simon <artysimon@gmail.com>
2023-08-16 22:30:24 -0400

    Change mobile menu for better accessibility

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
 import shortlyLogo from "../../assets/logo.svg";
 import HamburgerMenu from "./HamburgerIcon";
 
 function Header() {
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => {
+    const menuExpanded =
+      menuRef.current.getAttribute("aria-expanded") === "true";
+
+    if (menuExpanded) {
+      menuRef.current.setAttribute("aria-expanded", false);
+    } else {
+      menuRef.current.setAttribute("aria-expanded", true);
+    }
+  };
+
   return (
     <>
-      <header className="container relative flex items-center gap-10 py-10 bg-white">
+      <header className="container relative flex items-center gap-10 py-8 bg-white">
         <a href="#" target="_blank">
-          <img src={shortlyLogo} className="md:w-34" alt="Shortly Logo" />
+          <img
+            src={shortlyLogo}
+            className="w-[7.8rem] md:w-34"
+            alt="Shortly Logo"
+          />
         </a>
 
-        <input type="checkbox" className="hidden peer" name="menu" id="menu" />
-        <label htmlFor="menu" className="ml-auto cursor-pointer md:hidden">
+        <button
+          onClick={toggleMenu}
+          className="p-1 ml-auto md:hidden hover:bg-gray-100 hover:outline-2 hover:rounded-md"
+        >
+          <span className="sr-only">Menu</span>
           <HamburgerMenu />
-        </label>
+        </button>
 
         <nav
+          ref={menuRef}
           role="navigation"
-          className="absolute hidden py-10 text-center peer-checked:block md:block bg-primary-dark-violet rounded-xl top-24 left-8 inset-x-8 px-7 md:w-full md:static md:bg-transparent md:rounded-none md:top-auto md:left-auto md:inset-auto md:px-0 md:py-0 md:text-left "
+          aria-expanded="false"
+          className="absolute py-10 text-center mobile__menu md:block bg-primary-dark-violet rounded-xl top-[6.7rem] left-8 inset-x-8 px-7 md:w-full md:static md:bg-transparent md:rounded-none md:top-auto md:left-auto md:inset-auto md:px-0 md:py-0 md:text-left "
         >
           <ul className="flex flex-col gap-8 text-xl md:text-sm md:items-center md:flex-row">
             <li>
