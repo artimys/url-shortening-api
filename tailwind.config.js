e29b18ad5f935a1cbb58b138de918d7bf142e5cc
Arturo Simon <artysimon@gmail.com>
2023-08-16 23:10:35 -0400

    Disable 2xl screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      // "2xl": "1536px", // disabling 2xl breakpoint
+    },
+
     extend: {
       fontFamily: {
         poppins: "Poppins, sans-serif", // font-poppins
