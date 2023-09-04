4ad8214011cc9a80b77dd44ae56b0d48580dc1ed
Arturo Simon <artysimon@gmail.com>
2023-09-03 22:41:34 -0400

    Replace useRef with useState to toggle Menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,19 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import shortlyLogo from "../../assets/logo.svg";
 import HamburgerMenu from "../menu/HamburgerIcon";
+import CloseIcon from "../menu/CloseIcon";
 
 function Header() {
-  const menuRef = useRef(null);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    const menuExpanded =
-      menuRef.current.getAttribute("aria-expanded") === "true";
-
-    if (menuExpanded) {
-      menuRef.current.setAttribute("aria-expanded", false);
-    } else {
-      menuRef.current.setAttribute("aria-expanded", true);
-    }
+  const toggleMenuHandler = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
     <>
       <header className="container relative flex items-center gap-10 py-8 bg-white">
-        <a href="#" target="_blank">
+        <a href="#">
           <img
             src={shortlyLogo}
             className="w-[7.8rem] md:w-34"
@@ -28,18 +22,17 @@ function Header() {
         </a>
 
         <button
-          onClick={toggleMenu}
-          className="p-1 ml-auto md:hidden hover:bg-gray-100 hover:outline-2 hover:rounded-md"
+          onClick={toggleMenuHandler}
+          className="p-1 ml-auto md:hidden hover:bg-gray-100 hover:outline-2"
         >
           <span className="sr-only">Menu</span>
-          <HamburgerMenu />
+          {menuOpen ? <CloseIcon /> : <HamburgerMenu />}
         </button>
 
         <nav
-          ref={menuRef}
           role="navigation"
-          aria-expanded="false"
-          className="absolute py-10 text-center mobile__menu md:block bg-primary-dark-violet rounded-xl top-[6.7rem] left-8 inset-x-8 px-7 md:w-full md:static md:bg-transparent md:rounded-none md:top-auto md:left-auto md:inset-auto md:px-0 md:py-0 md:text-left "
+          aria-expanded={menuOpen}
+          className="absolute z-10 py-10 text-center mobile__menu md:block bg-primary-dark-violet rounded-xl top-[6.1rem] left-8 inset-x-8 px-7 md:w-full md:static md:bg-transparent md:rounded-none md:top-auto md:left-auto md:inset-auto md:px-0 md:py-0 md:text-left "
         >
           <ul className="flex flex-col gap-8 text-xl md:text-sm md:items-center md:flex-row">
             <li>
@@ -71,7 +64,7 @@ function Header() {
               <div className="w-full h-px bg-neutral-grayish-violet md:hidden"></div>
             </li>
 
-            <li className="md:self-en">
+            <li className="">
               <a
                 className="text-white hover:text-primary md:text-neutral-grayish-violet md:hover:text-primary-dark-violet"
                 href="#"
